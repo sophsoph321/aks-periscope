afde79916d15d6e78f56b1d932685abda7b02423
Tatsinnit <tamishra@microsoft.com>
2021-05-20 00:09:28 +1200

    simplify few things to keep 1 yaml file.

--- pkg/collector/helm_collector.go
+++ pkg/collector/helm_collector.go
@@ -1,9 +1,10 @@
 package collector
 
 import (
+	"path/filepath"
+
 	"github.com/Azure/aks-periscope/pkg/interfaces"
 	"github.com/Azure/aks-periscope/pkg/utils"
-	"path/filepath"
 )
 
 // HelmCollector defines a Helm Collector struct
@@ -30,34 +31,10 @@ func (collector *HelmCollector) Collect() error {
 		return err
 	}
 
-	output, err := utils.RunCommandOnContainer("apk", "add", "curl", "openssl", "bash", "--no-cache")
-	if err != nil {
-		return err
-	}
-
-	output, err = utils.RunCommandOnContainer("curl", "-fsSl", "-o", "get_helm.sh", "https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3")
-	if err != nil {
-		return err
-	}
-
-	output, err = utils.RunCommandOnContainer("chmod", "+x", "get_helm.sh")
-	if err != nil {
-		return err
-	}
-
-	output, err = utils.RunCommandOnContainer("./get_helm.sh")
-	if err != nil {
-		return err
-	}
-
 	helmListFile := filepath.Join(rootPath, "helm_list")
-	output, err = utils.RunCommandOnContainer("helm", "list", "--all-namespaces")
+	output, err := utils.RunCommandOnContainer("helm", "list", "--all-namespaces")
 	if err != nil {
-		errorMessage := err.Error()
-		err = utils.WriteToFile(helmListFile, errorMessage)
-		if err != nil {
-			return err
-		}
+		return err
 	}
 
 	err = utils.WriteToFile(helmListFile, output)
@@ -70,11 +47,7 @@ func (collector *HelmCollector) Collect() error {
 	helmHistoryFile := filepath.Join(rootPath, "helm_history")
 	output, err = utils.RunCommandOnContainer("helm", "history", "-n", "default", "azure-arc")
 	if err != nil {
-		errorMessage := err.Error()
-		err = utils.WriteToFile(helmHistoryFile, errorMessage)
-		if err != nil {
-			return err
-		}
+		return err
 	}
 
 	err = utils.WriteToFile(helmHistoryFile, output)
